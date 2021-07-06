0f85f6121ae3a3678d48b6979117f8c4aede8984
mutianjie <mutianjie@baidu.com>
2021-07-06 13:21:16 +0800

    Hello, world

--- src/main/java/com/nanxiaoqiao/community/controllers/GreetingController.java
+++ src/main/java/com/nanxiaoqiao/community/controllers/GreetingController.java
@@ -1,7 +1,22 @@
-package com.nanxiaoqiao.community.controllers;/**
-  *@Author mutianjie
-  *@Date 2021/7/6 12:56 下午
-  *@Version 1.0
-**/
+package com.nanxiaoqiao.community.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.View;
+
+/**
+ * @Author mutianjie
+ * @Date 2021/7/6 12:56 下午
+ * @Version 1.0
+ **/
+@Controller
 public class GreetingController {
+    @RequestMapping("/greet")
+    public String greet(@RequestParam(name = "name", required = false, defaultValue = "World")String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
 }
